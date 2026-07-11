97f5031ee19510c01104f3e987adcd32432c2d4a
Joshua J <jj4440024@gmail.com>
2026-07-11 12:11:50 +0530

    Create Search Registration component

---
+++ src/components/SearchRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchRegistration = () => {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">Search Registration</h2>
+    </div>
+  )
+}
+
+export default SearchRegistration
