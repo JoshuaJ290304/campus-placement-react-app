50e7068268158a15b8efe8008edcaaa1f2625124
Joshua J <jj4440024@gmail.com>
2026-07-11 12:19:25 +0530

    Fetch registration data using Fetch API

--- src/components/ViewRegistration.jsx
+++ src/components/ViewRegistration.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const ViewRegistration = () => {
-  return (
-    <div className="container mt-5">
-      <h2 className="text-center">View All Registrations</h2>
-    </div>
-  )
-}
 
-export default ViewRegistration
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+
+        fetch("https://raw.githubusercontent.com/anishpdm/demo-json-api/refs/heads/main/placement.json")
+            .then(response => response.json())
+            .then(data => setStudents(data));
+
+    }, []);
+
+    return (
+        <div className="container mt-5">
+            <h2 className="text-center">
+                View All Registrations
+            </h2>
+
+            <p>Total Students : {students.length}</p>
+
+        </div>
+    );
+};
+
+export default ViewRegistration;
