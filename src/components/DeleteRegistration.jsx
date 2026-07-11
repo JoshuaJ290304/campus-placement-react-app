e71e3ceaa787857c2ff1883c7fdf778e770ba5b5
Joshua J <jj4440024@gmail.com>
2026-07-11 12:17:42 +0530

    Create Delete Registration component

---
+++ src/components/DeleteRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteRegistration = () => {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">Delete Registration</h2>
+    </div>
+  )
+}
+
+export default DeleteRegistration
