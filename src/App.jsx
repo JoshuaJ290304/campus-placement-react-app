0b5a642b0ea69e36258ee37ce87a177f4a6bae9d
Joshua J <jj4440024@gmail.com>
2026-07-11 12:25:00 +0530

    Create Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import AddRegistration from "./components/AddRegistration";
+import SearchRegistration from "./components/SearchRegistration";
+import DeleteRegistration from "./components/DeleteRegistration";
+import ViewRegistration from "./components/ViewRegistration";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      gi
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<AddRegistration />} />
+        <Route path="/search" element={<SearchRegistration />} />
+        <Route path="/delete" element={<DeleteRegistration />} />
+        <Route path="/view" element={<ViewRegistration />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
