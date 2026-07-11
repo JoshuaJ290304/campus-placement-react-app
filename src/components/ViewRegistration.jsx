3299c15926a98e0a2b9ce41786d64b16942ffc21
Joshua J <jj4440024@gmail.com>
2026-07-11 12:19:11 +0530

    Create View Registration component

---
+++ src/components/ViewRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewRegistration = () => {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">View All Registrations</h2>
+    </div>
+  )
+}
+
+export default ViewRegistration
