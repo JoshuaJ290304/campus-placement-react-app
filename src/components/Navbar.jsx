07147c47cde5d9d13ae12e08f54d8450336ba9a5
Joshua J <jj4440024@gmail.com>
2026-07-11 12:25:17 +0530

    Add Bootstrap navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,59 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
       <div className="container">
-        <span className="navbar-brand mb-0 h1">
+
+        <Link className="navbar-brand" to="/">
           Campus Placement
-        </span>
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+
+          <ul className="navbar-nav ms-auto">
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Add Registration
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/search">
+                Search
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/delete">
+                Delete
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/view">
+                View All
+              </Link>
+            </li>
+
+          </ul>
+
+        </div>
+
       </div>
+
     </nav>
   )
 }
